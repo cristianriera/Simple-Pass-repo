2e0e7f093dfbc7e4c5291610148cc535d509fef3
cristianriera <7criera@stpatrick.org>
2017-04-26 11:24:56 -0500

    Add files via upload

---
+++ SimplePassword-master/simplepass.js
@@ -0,0 +1,17 @@
+//type checkString below
+function checkstring(str){
+  if(str.length>=8){
+    return str;
+  }
+  else if (str.length<=20){
+    return str;
+  }
+  else {
+    throw RangeError("You got the wrong Number!!");
+    }
+  }
+}
+function getString()
+
+
+//type getString below
